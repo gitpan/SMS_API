51322e81fdbc41faecbed6667bc9dcc2eb7a6cb3
Abhishek Jain <subs@ejain.com>
2005-06-19 10:56:09 +0000

    Import of GOYALI/SMS_API_3_01 from CPAN.
    gitpan-cpan-distribution: SMS_API
    gitpan-cpan-version:      3_01
    gitpan-cpan-path:         GOYALI/SMS_API_3_01.tar.gz
    gitpan-cpan-author:       GOYALI
    gitpan-cpan-maturity:     released

--- API.pm
+++ API.pm
@@ -3,30 +3,27 @@ package SMS::API;
 #################################################################################
 #
 #
-#             GOYALI.COM
+#             SMS1.in Goyali.com webbee.biz eMailOnMove.com
 #
 #################################################################################
-#NOTE: Distributing this script is permitted but requires a licence. Visit www.goyali.com/contact.htm for requesting
+#NOTE: Distributing this script is permitted but requires a licence. Visit www.sms1.in/contact.html for requesting
 #that . Provide the reason for distributing the script ie. for which application do you want to use the cript. You will be responded within hours.
-#Disclaimer: This program is distributed as it is and the author or goyali.com does not claim any responsibilities for the successful operation of this program or that we are not sure that it will or can cause any abnormality in your computer. However during our testing no such problem occured.
-#For a detailed policy visit www.goyali.com/policy.htm
+#Disclaimer: This program is distributed as it is and the author or sms1.in does not claim any responsibilities for the successful operation of this program or that we are not sure that it will or can cause any abnormality in your computer. However during our testing no such problem occured.
+#For a detailed policy visit www.sms1.in/policy.htm
 #
 #
 #This script is Prepared by ,
 #Abhsihek jain
 #CEO and Chief Programmer
-#Goyali.com
+#SMS1.in
 #
-
-require 5.000;
-require Exporter;
+use 5.006;
 use strict;
 use warnings;
 
-use Carp;
+require Exporter;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(&send_sms_email &sms_status &sms_send);
 
 
 our %EXPORT_TAGS = ( 'all' => [ qw(
@@ -35,81 +32,53 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our $VERSION = '2.01';
+our @EXPORT = qw();
+our $VERSION = '3.01';
 
 
-sub send_sms_email{
-        my ($user,$password,$to,$sender,$message,$email)=($_[0],$_[1],$_[2],$_[3],$_[4],$_[5]);
-        my $mailprog = "/usr/sbin/sendmail";
-        open (MAIL, "|$mailprog -oi -t") or die "Can't open pipe to $mailprog : \n";
-        print MAIL "To: smsmessaging\@goyali.com\n";
-        if($email &&($email =~ /\@/)){ print MAIL "From: $email\n";}else{print MAIL "From: webmaster\@goyali.com\n";}
-        print MAIL "Subject: SMS Gateway for $user\n";
-        print MAIL "X-Loop: project\@ejain.com\n";
-        print MAIL "User=$user\nPASSWORD=$password\nTO=$to\nSENDER=$sender\nREPLY=$email\nMESSAGE=$message";
-        close MAIL;
+# Preloaded methods go here.
+use HTTP::Request::Common qw(POST);
+use LWP::UserAgent;
+use XML::Simple;
+my $ua = LWP::UserAgent->new;
+sub new {
+    my ($self);
+    my $class = shift;
+    my (%hash) = @_;
+    $self = bless {
+        'userName' => $hash{userName},
+        'password' => $hash{password},
+        'messageId'=> $hash{messageId},
+        'messageText' => $hash{messageText},
+        'from' =>$hash{from},
+        'to' =>$hash{to},
+         }, $class;
+    return $self;
 }
 
-sub sms_status{
-     my ($user,$password,$messageid_id)=($_[0],$_[1],$_[2]);
-#     my ($user,$password,$messageid_id)=@_;
-    if(!$user||!$password||!$messageid_id){return 0;}
-     my $xml_send1=<<EOF;
-<?xml version="1.0" encoding="ISO-8859-1"?>
-<RequestStatus User='$user' Password='$password'>
-<messageId ID='$messageid_id'/>
-</RequestStatus>
-EOF
-  use HTTP::Request::Common qw(POST);
-  use LWP::UserAgent;
-  my $ua = LWP::UserAgent->new;
-  my $post_name='http://www.goyali.com/cgi-bin/services/sms_gateway/status.cgi';
-  my $req = POST "$post_name",
-                    [
-                        Data => "$xml_send1",
-                        action => "status"  ,
-                        submit => "send"
-                    ];
-     my $res=$ua->request($req);
-  my $xml_ret=$res->content;
-#  print $xml_ret;
-   if ($res->is_success) {
-  } else {
-      return '0';
-  }
-
-
-    my $config = XMLin($xml_ret);
-  my $error_code=$config->{'error'}->{'code'};
-  my $delivery_status_code=$config->{'delivery-status'}->{'code'};
-  if($error_code){return 0;}
-  elsif($delivery_status_code){return "$delivery_status_code";}
-  else {return '0';}
-}
-
-sub sms_send{
-     my ($user,$password,$to,$message,$from)=($_[0],$_[1],$_[2],$_[3],$_[4]);
-    if(!$user||!$password||!$to||!$message||!$from){return 0;}
-    if(!$from){
-      $from='GOYALI.COM';
+sub send{
+  my $self = shift;
+  if(!$self->{userName}||!$self->{password}||!$self->{to}||!$self->{messageText}){return 0;}
+    if(!$self->{from}){
+      $self->{from}='SMS1';
     }
- $message=substr $message,0,159;
-  $message=~ s/\&/\&amp;/gi;
-   $message=~ s/\</\&lt;/gi;
- $message=~ s/\'/\&apos;/gi;
+ $self->{messageText}=substr $self->{messageText},0,159;
+ $self->{messageText}=~ s/\&/\&amp;/gi;
+ $self->{messageText}=~ s/\</\&lt;/gi;
+ $self->{messageText}=~ s/\'/\&apos;/gi;
 
   use HTTP::Request::Common qw(POST);
   use LWP::UserAgent;
   my $ua = LWP::UserAgent->new;
   my $xml_send1=<<EOF;
 <?xml version="1.0" encoding="ISO-8859-1"?>
-<Messaging User='$user' Password='$password'>
-<Sender PutSender='$from'/>
-<Message Text='$message'/>
-<Receiver PutReceiver='$to'/>
+<Messaging User='$self->{userName}' Password='$self->{password}'>
+<Sender PutSender='$self->{from}'/>
+<Message Text='$self->{messageText}'/>
+<Receiver PutReceiver='$self->{to}'/>
 </Messaging>
 EOF
-  my $post_name='http://www.goyali.com/cgi-bin/services/sms_gateway/send.cgi';
+  my $post_name='http://www.sms1.in/cgi-bin/services/sms/send.sms';
   my $req = POST "$post_name",
                     [
                         Data => "$xml_send1",
@@ -118,11 +87,10 @@ EOF
                     ];
   my $res=$ua->request($req);
   my $xml_ret=$res->content;
-   if ($res->is_success) {
-  } else {
+   unless ($res->is_success) {
       return '0';
   }
-  use XML::Simple;
+
   my $config = eval { XMLin($xml_ret) };
   return '0' if($@);
   my $ok=$config->{completelyOk};
@@ -131,77 +99,130 @@ EOF
   my $error_text=$config->{Error}->{'errorText'};
   my $error_code_send=$config->{Error}->{'errorCode'};
   my $message_id_error=$config->{Error}->{'messageID'};
-  if($ok eq 'no' || !$ok){
-    return '0';
-  }else{
-     return "$message_id_ok";
+  if($message_id_ok){
+   return $message_id_ok;
+   }else{
+    return "0";
   }
   return '0';
 }
 
+sub status {
+    my $self = shift;
+    
+    if(!$self->{userName}||!$self->{password}||!$self->{messageId}){
+#      print "Sorry no userName or password or Message Id specified";
+      return '0';
+    }
+     my $xml_send1=<<EOF;
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<RequestStatus User='$self->{userName}' Password='$self->{password}'>
+<messageId ID='$self->{messageId}'/>
+</RequestStatus>
+EOF
+  my $post_name='http://www.sms1.in/cgi-bin/services/sms/status.sms';
+  my $req = POST "$post_name",
+                    [
+                        Data => "$xml_send1",
+                        action => "status"  ,
+                        submit => "send"
+                    ];
+   my $res=$ua->request($req);
+   my $xml_ret=$res->content;
+
+   unless($res->is_success) {
+      return '0';
+  }
+
+
+  my $config = XMLin($xml_ret);
+  my $error_code=$config->{'error'}->{'code'};
+  my $delivery_status_code=$config->{'delivery-status'}->{'code'};
+  if($error_code){return 0;}
+  elsif($delivery_status_code){return "$delivery_status_code";}
+  else {return '0';}
+}
+
 1;
-
 __END__
-
+# Below is stub documentation for your module. You better edit it!
 
 =head1 NAME
 
-SMS::API - A module to send SMS using the goyali.com servers
+SMS::API - A module to send SMS using the sms1.in servers
 
 =head1 SYNOPSIS
-
+  use XML::Simple;
   use SMS::API;
-  $message_id=sms_send("$user","$password","$to","$message","$from");
+                        #To send SMS Replace with username and password and message text. You can get User name by visiting http://www.sms1.in
+    my $sms = SMS::API->new(
+    'userName' => "$user_name",
+    'password' => "$password",
+    'to' =>  "$to",      #Substitute by a valid International format mobile number to whom you wanted to send SMS. eg. 919811111111
+    '$from' => "$from",  #Optional
+    'messageText'=>"$message_text", #Max 160 characters Message.
+      );
+$send = $sms->send;
 
-  $sent_status=sms_status("$user","$password","$message_id");
+                  #To check the status of the messages sent;
+    my $sms = SMS::API->new(
+    'userName' => $userName,
+    'password' => $password,
+    'messageId'=>$messageId,
+      );
+    $status = $sms->status; #$status will be sent to 1 if message hacs been received
+
+
 
 =head1 DESCRIPTION
 
-This is a module for sending the SMS by integrating with the servers of http://www.goyali.com.
+This is a module for sending the SMS by integrating with the servers of http://www.sms1.in
 
-For a solution of directly sending the SMS via internet or through SMS visit http://www.goyali.com
+For a solution of directly sending the SMS via internet or through SMS visit http://www.sms1.in
+In case of any problem whatsoever please do not hesitate to contact http://www.sms1.in/contact.html .We have an excellent team of customer support and you will be responded in hours.
 
-In case of any problem whatsoever please do not hesitate to contact http://www.goyali.com/contact.htm .We have an excellent team of customer support and you will be responded in hours.
+NOTE: You first need to register at http://www.sms1.in/signup.html (visit the URL and request a sms gateway account. You will given one within seconds and it is free)for using this module. On registering you will be given a user id and password and some free test credits. You can use the credits to send the SMS.
 
-NOTE: You first need to register at http://www.goyali.com/contact.htm (visit the URL and request a sms gateway account. You will given one within hours)for using this module. On registering you will be given a user id and password and some free test credits. You can use the credits to send the SMS.
+NOTE: We are very serious about this module and any errors performed are requested to be reported at http://www.sms1.in/contact.html
 
-NOTE: We are very serious about this module and any errors performed are requested to be reported at http://www.goyali.com/contact.htm
+NOTE: This module has a dependency over XML::Simple and that must be installed to properly send the SMSes . It can be optained easily from cpan.
 
 At the moment these methods are implemented:
 
 =over 4
 
-=item C<sms_send("$user","$password","$to","$message","$from")>
+=item C<new>
+A constructor
+
+=item C<send>
 
 This method send an SMS. The parameters sent are :
-$user , $password = The user name and Password given to you at the time of signup.
-$message= The message you wish to send.
-$from= If your account supports dynamic sender id then this parameter will tell what will be the from id as seen by the mobile.
+userName , password = The user name and Password given to you at the time of signup.
+messageText = The message you wish to send.
+from = If your account supports dynamic sender id then this parameter will tell what will be the from id as seen by the mobile.
+to = The mobile number whom you wish to send SMS to in international  format eg. 919811111111 .
 
 On successful sending the SMS the function returns the unique message id which can be used to track the delivery status of the SMS otherwise it will return 0.
 
-=item C<sms_status("$user","$password","$message_id")>
+=item C<status>
 
 This method gives the status of the SMS ie. whether it has been sent or not. The parameters sent are :
-$user , $password = The user name and Password given to you at the time of signup.
-$message_id= The message id whose status you want to determine.
 
 The function returns the status of message ie. 1 if message has been received by the mobile , 0 if it has not been delivered yet ie. Failure or Retrying.
 
+You can also visit the SMS1.in to check the status by logging on to your account.
+
 =back
-
-=head2 EXPORT
-
-send_sms_email , sms_send , sms_status .
 
 
 =head1 AUTHOR
 
-Abhishek jain<lt>goyali@cpan.org<gt>
+<Abhishek jain>
+goyali at cpan.org
 
 =head1 SEE ALSO
 
-http://www.goyali.com
+http://www.sms1.in
 
 
 =cut
---
+++ t/use.t
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl -w
+use strict;
+use Test;
+BEGIN { plan tests => 1}
+
+use SMS::API;
+use XML::Simple;
+ok(1);
+exit;
+
